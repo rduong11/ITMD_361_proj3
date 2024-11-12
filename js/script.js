a1b2d885d8d4bc2bc4c566824caba7a84d5e0911
rduong11 <raymondduong12@gmail.com>
2024-11-12 13:55:22 -0600

    image slider implemented into media

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
+//dark mode feature
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
 
+//image slider
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+initSlider();
+
+function initSlider() {
+
+  if(slides.length > 0) {
+      slides[slideIndex].classList.add("displaySlide");
+      intervalId = setInterval(nextSlide, 10000);
+  }
+
+}
+
+function showSlide(index) {
+
+  if(index >= slides.length) {
+      slideIndex = 0;
+  }
+  else if(index < 0) {
+      slideIndex = slides.length - 1;
+  }
+
+    slides.forEach(slide => {
+      slide.classList.remove("displaySlide");
+    });
+
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+
+//map api
 function initMap(){
 
   const mapDetails = {
